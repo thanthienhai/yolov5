35b10936ac64e13938b2a19574d3d6f669084f52
Than Thien <50171638+thanthienhai@users.noreply.github.com>
2024-04-08 11:54:08 +0700

    Fix count function
    Sửa lỗi hàm  count_object_each_class_id(class_ids) chỉ đến các id class được detect, ta cần phải đếm tất cả các class để khi dùng filter không bị lỗi không thấy id trong class_ids mới.

--- main.py
+++ main.py
@@ -98,12 +98,10 @@ Quy ước:
 [  '0',       '1',      '2',      '3',      '4',      '5']
 '''
 def count_object_each_class_id(class_ids):
-  frequency = {}
+  frequency = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
   for id in class_ids:
-    if id in frequency:
+    if 0 <= id <= 5:
       frequency[id] += 1
-    else:
-      frequency[id] = 1
   return frequency
 
 #Hàm lọc box theo id tương ứng: 
